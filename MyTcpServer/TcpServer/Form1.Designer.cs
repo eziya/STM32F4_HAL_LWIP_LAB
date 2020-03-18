5d57b43b6579135b2c4f17fc720f88cdff246c7f
JIHOON LEE <eziya76@gmail.com>
2020-03-18 22:40:16 +0900

    update
    update

---
+++ MyTcpServer/TcpServer/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TcpServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lbxMessage = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(125, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start Server";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(143, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(125, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop Server";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lbxMessage
+            // 
+            this.lbxMessage.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lbxMessage.FormattingEnabled = true;
+            this.lbxMessage.ItemHeight = 12;
+            this.lbxMessage.Location = new System.Drawing.Point(0, 52);
+            this.lbxMessage.Name = "lbxMessage";
+            this.lbxMessage.Size = new System.Drawing.Size(515, 148);
+            this.lbxMessage.TabIndex = 2;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(274, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(125, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear Messages";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 200);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lbxMessage);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox lbxMessage;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ MyTcpServer/TcpServer/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TcpServer
+{
+    public partial class Form1 : Form
+    {
+        private const int LISTEN_PORT = 1234;
+
+        private TcpListener server;
+        private Thread serverThread;
+        private int connectionCnt;
+
+        void AddListBox(ListBox listbox, String text)
+        {
+            if(listbox.InvokeRequired)
+            {
+                listbox.BeginInvoke(new Action(() => { 
+                    if(listbox.Items.Count > 1000)
+                    {
+                        listbox.Items.Clear();
+                    }
+                    listbox.Items.Add(text);
+                    listbox.SelectedIndex = listbox.Items.Count - 1;
+                }));
+            }
+            else
+            {
+                if (listbox.Items.Count > 1000)
+                {
+                    listbox.Items.Clear();
+                }
+                listbox.Items.Add(text);
+                listbox.SelectedIndex = listbox.Items.Count - 1;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(server != null)
+            {
+                server.Stop();
+            }
+
+            Application.Exit();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            serverThread = new Thread(delegate () {
+
+                try
+                {
+                    connectionCnt = 0;
+                    server = new TcpListener(IPAddress.Any, LISTEN_PORT);
+                    server.Server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                    server.Start();
+
+                    AddListBox(lbxMessage, "Server started to listen at port " + LISTEN_PORT);
+
+                    while (true)
+                    {
+                        byte[] buffer = new byte[256];
+                        int nRead = 0;                        
+
+                        AddListBox(lbxMessage, "Wait to accept the client...");
+                        TcpClient client = server.AcceptTcpClient();
+                        connectionCnt++;
+                        AddListBox(lbxMessage, connectionCnt + ": Client accepted...");
+                        
+                        NetworkStream ns = client.GetStream();
+
+                        nRead = ns.Read(buffer, 0, buffer.Length);
+                        AddListBox(lbxMessage, connectionCnt + ": Read " + nRead + " bytes from the client");
+
+                        if (nRead == 256)
+                        {
+                            if ((buffer[0] == 0xAE) && buffer[255] == 0xEA)
+                            {
+                                DateTime now = DateTime.Now;
+                                buffer[1] = 1;   //RESP
+                                buffer[2] = (byte)(now.Year - 2000); //year
+                                buffer[3] = (byte)now.Month; //month
+                                buffer[4] = (byte)now.Day; //day
+                                buffer[5] = (byte)now.Hour; //hour
+                                buffer[6] = (byte)now.Minute; //minute
+                                buffer[7] = (byte)now.Second; //second
+
+                                ns.Write(buffer, 0, buffer.Length);
+                                AddListBox(lbxMessage, connectionCnt + ": Write " + buffer.Length + " bytes to the client");
+                            }
+                        }                        
+
+                        client.Close();
+                        AddListBox(lbxMessage, connectionCnt + ": Close the session...");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }            
+            
+            });
+            serverThread.IsBackground = true;
+            serverThread.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(serverThread != null)
+                {
+                    server.Stop();
+                    serverThread.Join();
+                    serverThread = null;
+                    AddListBox(lbxMessage, "Stop to listen at " + LISTEN_PORT);
+                }
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lbxMessage.Items.Clear();
+        }
+    }
+}
